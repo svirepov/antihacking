5a09354566ffb8eac3a80e02d47f402210c184ed
Evgeny <tradelikebot@gmail.com>
2019-02-05 23:29:33 +0500

    Update application.php
    fix html injections and XSS

--- application.php
+++ application.php
@@ -1,15 +1,15 @@
 <?php
 
 $sendto   = "sales@antihacking.ru"; // почта, на которую будет приходить письмо
-$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
-$usertel = $_POST['telephone']; // сохраняем в переменную данные полученные из поля c телефонным номером
-$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
-$usertext = $_POST['text'];
+$username = htmlentities($_POST['name']);   // сохраняем в переменную данные полученные из поля c именем
+$usertel = htmlentities($_POST['telephone']); // сохраняем в переменную данные полученные из поля c телефонным номером
+$usermail = strip_tags($_POST['email']); // сохраняем в переменную данные полученные из поля c адресом электронной почты
+$usertext = htmlentities($_POST['text']);
 
 // Формирование заголовка письма
 $subject  = "Заявка";
-$headers  = "From: " . strip_tags($usermail) . "\r\n";
-$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers  = "From: " . $usermail . "\r\n";
+$headers .= "Reply-To: ". $usermail . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 
